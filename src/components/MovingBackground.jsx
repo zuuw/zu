d5547dd137e0ee08fa58c30e2c780d8ff9a09a91
Cody McLeod <codymcleod02@gmail.com>
2025-10-07 01:13:02 -0600

    fresh

---
+++ src/components/MovingBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MovingBackground() {
+
+    const randomColor = (1, 2, 3)
+  return (
+   <div className='flex justify-center self-center items-center' ></div>
+  )
+}
+
+export default MovingBackground
